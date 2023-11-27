97b655bdde87c569e1931bf0792e27895959fd25
Robin Schmidt <schmidtrobin02@gmail.com>
2023-11-27 23:33:13 +0100

    feat: add basic custom dropzone

--- src/app/recipe/create/ImageUploader.tsx
+++ src/app/recipe/create/ImageUploader.tsx
@@ -1,7 +1,64 @@
-import { Button, Image } from "@nextui-org/react";
+import { Button, Card, CardBody, Image } from "@nextui-org/react";
 import { useFieldArray, useFormContext } from "react-hook-form";
 import { UploadButton } from "~/utils/uploadthing";
+import { generateClientDropzoneAccept } from "uploadthing/client";
+import { useDropzone } from "@uploadthing/react/hooks";
+import { useUploadThing } from "~/app/lib/uploadthing";
+import { SetStateAction, useCallback, useState } from "react";
+import { CardHeader } from "@nextui-org/card";
 
+function UploadButton2() {
+  const [files, setFiles] = useState<File[]>([]);
+  const onDrop = useCallback((acceptedFiles: SetStateAction<File[]>) => {
+    setFiles(acceptedFiles);
+  }, []);
+  const { startUpload, permittedFileInfo } = useUploadThing(
+    "recipeImagesUploader",
+    {
+      onClientUploadComplete: () => {
+        //TODO: Change this to toast notifications
+        alert("uploaded successfully!");
+      },
+      onUploadError: () => {
+        alert("error occurred while uploading");
+      },
+      onUploadBegin: () => {
+        alert("upload has begun");
+      },
+    },
+  );
+
+  const fileTypes = permittedFileInfo?.config
+    ? Object.keys(permittedFileInfo?.config)
+    : [];
+
+  const { getRootProps, getInputProps } = useDropzone({
+    onDrop,
+    accept: fileTypes ? generateClientDropzoneAccept(fileTypes) : undefined,
+  });
+
+  return (
+    <Card>
+      <CardHeader className="flex justify-center">Upload Images</CardHeader>
+      <CardBody className="p-4">
+        <div
+          {...getRootProps()}
+          className="rounded border-1 border-dashed border-black"
+        >
+          <input {...getInputProps()} />
+          <div>
+            {files.length > 0 && (
+              <Button onClick={() => startUpload(files)}>
+                Upload {files.length} files
+              </Button>
+            )}
+          </div>
+          Drop files here!
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
 export default function ImageUploader() {
   const { control, watch, getValues } = useFormContext();
 
@@ -29,6 +86,7 @@ export default function ImageUploader() {
           </Button>
         </div>
       ))}
+      <UploadButton2 />
       <UploadButton
         endpoint="recipeImagesUploader"
         onClientUploadComplete={(res) => {
