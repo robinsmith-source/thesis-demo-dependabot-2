8286d67f0f02f325549653193ad07f61ba70363b
Deniz Gazitepe <dg102@hdm-stuttgart.de>
2023-12-18 19:16:46 +0100

    feat: add Accordion for Filters when Component is rendered on recipe/search

--- src/app/_components/RecipeSearchbar.tsx
+++ src/app/_components/RecipeSearchbar.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { useState, type KeyboardEvent } from "react";
-import { Button, Input } from "@nextui-org/react";
-import { FaMagnifyingGlass } from "react-icons/fa6";
+import { Accordion, AccordionItem, Button, Input } from "@nextui-org/react";
+import { FaMagnifyingGlass, FaFilter } from "react-icons/fa6";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { useDebouncedCallback } from "use-debounce";
 
@@ -20,30 +20,34 @@ export default function RecipeSearchbar() {
   const searchParams = useSearchParams();
   const [searchQuery, setSearchQuery] = useState<queryInput>();
   const pathname = usePathname();
-  const { replace, push } = useRouter();
+  const router = useRouter();
+  type HandleSearchFunction = (searchFilters: queryInput) => void;
 
   // debounce the search function to save resources
-  const handleSearch = useDebouncedCallback((searchFilters: queryInput) => {
-    const params = new URLSearchParams(searchParams);
+  const handleSearch: HandleSearchFunction = useDebouncedCallback(
+    (searchFilters: queryInput) => {
+      const params = new URLSearchParams(searchParams);
 
-    console.log("searchFilters: ", searchFilters);
+      console.log("searchFilters: ", searchFilters);
 
-    if (searchFilters) {
-      const { name, difficulty, labels, tags, authorId } = searchFilters;
+      if (searchFilters) {
+        const { name, difficulty, labels, tags, authorId } = searchFilters;
 
-      name && params.set("name", name ?? "");
-      difficulty && params.set("difficulty", difficulty ?? "");
-      labels && params.set("labels", labels?.join(",") ?? "");
-      tags && params.set("tags", tags?.join(",") ?? "");
-      authorId && params.set("authorId", authorId ?? "");
-    }
+        name && params.set("name", name ?? "");
+        difficulty && params.set("difficulty", difficulty ?? "");
+        labels && params.set("labels", labels?.join(",") ?? "");
+        tags && params.set("tags", tags?.join(",") ?? "");
+        authorId && params.set("authorId", authorId ?? "");
+      }
 
-    if (pathname !== `/recipe/search`) {
-      push(`/recipe/search?${params.toString()}`);
-    } else {
-      replace(`${pathname}?${params.toString()}`);
-    }
-  }, 300); // debounce time in ms
+      if (pathname !== `/recipe/search`) {
+        router.push(`/recipe/search?${params.toString()}`);
+      } else {
+        router.replace(`${pathname}?${params.toString()}`);
+      }
+    },
+    300,
+  ); // debounce time in ms
 
   function handleKeyDown(event: KeyboardEvent<HTMLInputElement>) {
     if (event.key === "Enter") {
@@ -52,24 +56,38 @@ export default function RecipeSearchbar() {
   }
 
   return (
-    <Input
-      fullWidth
-      type="text"
-      value={searchQuery?.name ?? ""}
-      onChange={(event) =>
-        setSearchQuery({ ...searchQuery, name: event.target.value })
-      }
-      onKeyDown={handleKeyDown}
-      defaultValue={searchParams.get("name")?.toString()} // ensure that the input field is in sync with the URL
-      placeholder="Search recipes..."
-      endContent={
-        <Button
-          onClick={() => handleSearch(searchQuery)}
-          endContent={<FaMagnifyingGlass size={30} />}
-        >
-          Search
-        </Button>
-      }
-    />
+    <>
+      <Input
+        fullWidth
+        type="text"
+        value={searchQuery?.name ?? ""}
+        onChange={(event) =>
+          setSearchQuery({ ...searchQuery, name: event.target.value })
+        }
+        onKeyDown={handleKeyDown}
+        defaultValue={searchParams.get("name")?.toString()} // ensure that the input field is in sync with the URL
+        placeholder="Search recipes..."
+        endContent={
+          <Button
+            onClick={() => handleSearch(searchQuery)}
+            endContent={<FaMagnifyingGlass size={30} />}
+          >
+            Search
+          </Button>
+        }
+      />
+      {pathname === "/recipe/search" && (
+        <Accordion className="mt-0" disableIndicatorAnimation>
+          <AccordionItem
+            title="Advanced"
+            key="filters"
+            aria-label="filters"
+            indicator={<FaFilter />}
+          >
+            Content for accordion item 1
+          </AccordionItem>
+        </Accordion>
+      )}
+    </>
   );
 }
